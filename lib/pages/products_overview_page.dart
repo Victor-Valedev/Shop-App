2dd01aedcd5155094339c23989172ed6848939dc
Victor-Valedev <victorvaledev@gmail.com>
2025-04-23 10:16:11 -0300

    feat: Cria componente inicial de exibição dos produtos

---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:shopapp/components/product_item.dart';
+import 'package:shopapp/data/dummy_data.dart';
+import 'package:shopapp/models/product.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  ProductsOverviewPage({super.key});
+
+  final List<Product> loadedProducts = dummyProducts;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Minha loja')),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, index) => ProductItem(product: loadedProducts[index]),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
+    );
+  }
+}
