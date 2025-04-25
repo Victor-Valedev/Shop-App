1c5e60a5f9441c4a84bf199097edae8c3048a2de
Victor-Valedev <victorvaledev@gmail.com>
2025-04-25 09:14:35 -0300

    refactor: atualiza valor da rota duplicada

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -2,5 +2,5 @@ class AppRoutes {
   static const HOME = '/';
   static const PRODUCT_DETAIL = '/product-detail';
   static const CART_SHOP = '/cart-shop';
-  static const ORDERS = '/cart-shop';
+  static const ORDERS = '/orders';
 }
